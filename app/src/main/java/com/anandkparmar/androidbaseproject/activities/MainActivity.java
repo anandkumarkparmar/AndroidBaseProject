fe44686e4cb70c291dd2172a1b31b0947f68b877
anandkumarkparmar <anandkumar.k.parmar@gmail.com>
2017-11-25 12:47:47 +0530

    styles added

--- app/src/main/java/com/anandkparmar/androidbaseproject/activities/MainActivity.java
+++ app/src/main/java/com/anandkparmar/androidbaseproject/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.anandkparmar.androidbaseproject;
+package com.anandkparmar.androidbaseproject.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.anandkparmar.androidbaseproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
